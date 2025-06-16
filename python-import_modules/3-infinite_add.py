f70676f1587c99253899629b9f35ac27d8b01173
someonelse09 <burjuzada@gmail.com>
2025-06-16 13:23:39 +0000

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     count = len(argv)
     if count == 0:
-        print("0")
+        print(0)
     else:
         res = 0
         for i in range(count):
             res += int(argv[i])
-            print(res)
+        print(res)
