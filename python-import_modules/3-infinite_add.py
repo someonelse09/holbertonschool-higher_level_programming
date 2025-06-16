78fc8adaf3711e765c15596e23bc69dc4c4ea209
someonelse09 <burjuzada@gmail.com>
2025-06-16 13:20:20 +0000

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    count = len(argv)
+    if count == 0:
+        print("0")
+    else:
+        res = 0
+        for i in range(count):
+            res += int(argv[i])
+            print(res)
