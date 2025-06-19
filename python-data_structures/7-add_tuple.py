9db81a310f30ec24d367ef343d3c2f341c832ce3
someonelse09 <burjuzada@gmail.com>
2025-06-19 09:59:23 +0000

    7-add_tuple && 7-main

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,11 +2,16 @@
 def add_tuple(a=(), b=()):
     l1 = list(a)
     l2 = list(b)
-    if len(l1) > len(l2):
+    if len(l1) < 2:
         z_list = [0 for _ in range(len(l1) - len(l2))]
+        l1 = l1 + z_list
+    elif len(l2) < 2:
+        z_list = [0 for _ in range(len(l2) - len(l1))]
         l2 = l2 + z_list
-    elif len(l1) < len(l2):
-        l2 = l2[:len(l1)]
+    elif len(l1) > 2:
+        l1 = l1[:2]
+    elif len(l2) > 2:
+        l2 = l2[:2]
     i = 0
     while i < len(l1):
         l1[i] += l2[i]
