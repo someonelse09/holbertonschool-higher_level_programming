480fdbe68f114dddea6c1d83a0197e4d6c2bb0b1
someonelse09 <burjuzada@gmail.com>
2025-06-19 11:25:21 +0000

    11-delete_at && 11-main

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,9 +4,9 @@ def delete_at(my_list=[], idx=0):
         return my_list
     else:
         new_list = []
-        for i in my_list:
-            if i == my_list[idx]:
+        for i in range(len(my_list)):
+            if i == idx:
                 continue
             else:
-                new_list.append(i)
+                new_list.append(my_list[i])
     return new_list
