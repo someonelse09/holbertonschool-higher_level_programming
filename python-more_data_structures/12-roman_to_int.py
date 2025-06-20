13baa392fb49127c9e544264495b0c844079bdab
someonelse09 <burjuzada@gmail.com>
2025-06-20 19:42:53 +0000

    12-roman_to_int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
-        return None
+    if not isinstance(roman_string, str):
+        return 0
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
     prev_value = 0
