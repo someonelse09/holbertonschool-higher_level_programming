116fdebd1533e260e91e9354fdfa2a095d09ce1e
someonelse09 <burjuzada@gmail.com>
2025-06-20 14:52:37 +0000

    6-print_sorted_dictionary && 6-main

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    l = sorted(a_dictionary)
-    for x in l:
+    res = sorted(a_dictionary)
+    for x in res:
         print("{}: {}".format(x, a_dictionary.get(x)))
