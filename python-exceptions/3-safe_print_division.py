5215e168818f2e6092da2550a802ffc686d48164
someonelse09 <burjuzada@gmail.com>
2025-06-21 11:35:26 +0000

    3-safe_print_division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     res = float(0)
     try:
         res += float(a/b)
-    except:
+    except ZeroDivisionError:
         res = None
     finally:
         print("Inside result: {}".format(res))
