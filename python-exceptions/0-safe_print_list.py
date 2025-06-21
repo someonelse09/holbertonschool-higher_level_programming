63a6bfce7212e66a7e695b8a037b933363a242ac
someonelse09 <burjuzada@gmail.com>
2025-06-21 10:31:08 +0000

    0-safe_print_list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     for t in range(x):
         try:
             print(my_list[t], end="")
+            count += 1
         except IndexError:
-            None
+            break
+    print()
+    return count
