d4e74b868bcf9eda1f6b690775667e9321c001f5
someonelse09 <burjuzada@gmail.com>
2025-06-26 07:52:52 +0000

    0-rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ This module creates a rectangle class
 for geometric rectangle representation """
+
+
 class Rectangle:
     """ This class defines a rectangle
     without any method """
