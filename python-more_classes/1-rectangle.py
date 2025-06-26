95a566329d584220aa3ed286469c7287877abad3
someonelse09 <burjuzada@gmail.com>
2025-06-26 12:11:50 +0000

    1-rectangle && 1-main

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """ This is the initialization of the
         Rectangle class in order for us to generate an object from it """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
