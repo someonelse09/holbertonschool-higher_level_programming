06e02adc027a0c93458b544d4dd59e22eb0475dd
someonelse09 <burjuzada@gmail.com>
2025-06-26 13:19:24 +0000

    3-rectangle && 3-main

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" This module creates a rectangle class
+for geometric rectangle representation """
+
+
+class Rectangle:
+    """ This class  defines a Rectangle
+    and methods for its parameters: height and width
+    """
+
+    def __init__(self, width=0, height=0):
+        """ This is the initialization of the
+        Rectangle class in order for us to generate an object from it """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ This method allows us to get
+        the value of Rectangle's width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ This method serves as a width setter """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ This method is for getting
+        the value of Rectangle's height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ This method is defined for
+        setting a new value for the height """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ This method calculates
+        the area of the Rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ This method returns perimeter
+        based on the size of the Rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2*(self.__height + self.__width)
+
+    def __str__(self):
+        """ This method prints the
+        Rectangle object using hash symbol """
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        repictangle = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                repictangle += "#"
+            if i < self.__height - 1:
+                repictangle += "\n"
+        return repictangle
