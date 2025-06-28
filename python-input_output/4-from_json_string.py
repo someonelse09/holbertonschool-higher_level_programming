20ce39b4046e45e2fb1d8f25ea74b075b6560ab8
someonelse09 <burjuzada@gmail.com>
2025-06-28 11:38:08 +0000

    4-from_json_string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module includes the function from_json_string(my_str): """
+
+import json
+
+
+def from_json_string(my_str):
+    """ The function serves as a tool for
+    converting a json string into an object """
+
+    return json.loads(my_obj)
