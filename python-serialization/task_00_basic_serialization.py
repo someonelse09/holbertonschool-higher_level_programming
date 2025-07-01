d9200055cf88d19b3de677f8735c7c9a90d8d56d
someonelse09 <burjuzada@gmail.com>
2025-07-01 16:20:27 +0000

    task_00_basic_serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,45 +1,15 @@
-#!/usr/bin/python3
-"""
-Basic serialization module for converting Python dictionaries to JSON files
-and deserializing JSON files back to Python dictionaries.
-"""
+#!/usr/bin/env python3
 
 import json
 
 
 def serialize_and_save_to_file(data, filename):
-    """
-    Serialize a Python dictionary to JSON and save it to a file.
+    # Your code here to serialize and save data to the specified file
+    with open(filename, 'w') as file:
+        json.dump(data, file)
 
-    Args:
-        data (dict): A Python dictionary containing data to serialize
-        filename (str): The filename of the output JSON file
-
-    Returns:
-        None
-    """
-
-    try:
-        with open(filename, 'w', encoding="utf-8") as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
-    except (TypeError, ValueError) as e:
-        raise ValueError(f"Data is not JSON serializable: {e}")
 
 def load_and_deserialize(filename):
-    """
-    Load and deserialize JSON data from a file to recreate a Python dictionary.
-
-    Args:
-        filename (str): The filename of the input JSON file
-
-    Returns:
-        dict: A Python dictionary with the deserialized JSON data
-    """
-
-    try:
-        with open(filename, 'r', encoding="utf-8") as file:
-            json.load(file)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"The {filename} could not found")
-    except IOError as e:
-        raise IOError(f"Error happened while reading the {filename}: {e}")
+    # Your code here to load and deserialize data from the specified file
+    with open(filename, 'r') as file:
+        return json.load(file)
