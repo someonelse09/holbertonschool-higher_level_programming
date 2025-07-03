6d09ac5188bbe18c71a11de6792c23e2217b52dc
someonelse09 <burjuzada@gmail.com>
2025-07-03 14:19:55 +0000

    11-best_score

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- This is a script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
 SELECT score, name FROM second_table
-ORDER BY second_table DESC
 WHERE score >= 10
+ORDER BY second_table DESC
