a7f3da509a275a0d82473228677deea92dc81b34
someonelse09 <burjuzada@gmail.com>
2025-07-14 15:07:09 +0000

    5-save_to_json_file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module includes a function that writes an
+Object to a text file, using a JSON representation """
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """ with statement must be used
+    do not need to manage exceptions if the object can not be serialized.
+    do not need to manage file permission exceptions. """
+
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(my_obj)
